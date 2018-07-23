dd6335c59a89e07da331ac3dd17e05c1167f0d80
anirban07 <banirban@vmware.com>
2018-07-23 12:23:30 -0700

    Madified build.sh to not use pushd popd

--- integration/build.sh
+++ integration/build.sh
@@ -7,11 +7,13 @@ cd "$(dirname "$0")"
 G="$(git rev-parse --show-toplevel)"
 K="$G/integration/docker"
 
-pushd "$G/go-controller"
+# pushd "$G/go-controller"
+cd "$G/go-controller"
 make
-popd
+cd -
+# popd
 
 tar Cczf "$G/go-controller/_output/go/bin" "$K/ovn-go.tar.gz" .
 
 cd "$K"
-docker build -t vmware/photon-2.0-ovnkube:v0.1.0 .
+docker build -t vmware/photon-2.0-ovnkube:v1.1.0 .
